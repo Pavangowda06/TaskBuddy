b425cac87ce469b823323bd447d0026fcf31f286
Pavangowda06 <spavansdr@gmail.com>
2024-12-23 16:16:37 +0530

    Added New Files

--- taskbuddyap/src/App.js
+++ taskbuddyap/src/App.js
@@ -1,8 +1,11 @@
 import TaskForm from './Components/TaskForm';
-
+import './Style.css'
 function App() {
   return (
+    <>
+    <div className="App"></div>
     <TaskForm/>
+    </>
   );
 }
 
--- taskbuddyap/src/Components/TaskForm.jsx
+++ taskbuddyap/src/Components/TaskForm.jsx
@@ -2,18 +2,35 @@ import React, {useState} from 'react'
 
 export default function TaskForm() {
   const [task, setTask] = useState('');
+  const [priority, setPriority] = useState('Medium');
+  const [category, setCategory] = useState('General');
   return (
     <div>
-        <form>
+        <form className='task-form'>
+          <div id="inp">
             <input type='text'
             placeholder='Enter your text'
             required
             value = {task}
             onChange={(e) => setTask(e.target.value)}/>
+            <span><button>Add Task</button></span>
+            </div>
+            <div className='btns'>
+         
+              <select value={priority} onChange={(e)=>setPriority(e.target.value)}>
+                <option>Medium</option>
+                <option>Low</option>
+                <option>High</option>
+              </select>
 
-            <h1>{task}</h1>
-
+              <select value={category} onChange={(e)=>setCategory(e.target.value)}>
+                <option>General</option>
+                <option>Work</option>
+                <option>Personal</option>
+              </select>
+            </div>
         </form>
+        
     </div>
   )
 }
